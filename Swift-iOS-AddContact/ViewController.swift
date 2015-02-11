9239eef03c4396b875681e23adf304c05e15b0fe
Rudi Luis <rudiluis@Rudis-MBP.lan>
2015-02-11 14:32:30 +0000

    First commit

---
+++ Swift-iOS-AddContact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-iOS-AddContact
+//
+//  Created by Rudi Luis on 11/02/15.
+//  Copyright (c) 2015 Rudi Luis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
